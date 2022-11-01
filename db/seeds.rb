1567bdf5fd1b2c66ff050c834bb32e761359a403
Christoffer Nguyen <christofferhoang@gmail.com>
2022-11-01 11:19:09 +0200

    FEAT: Added images to item

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,17 +11,18 @@ User.destroy_all
   profile_url = "https://picsum.photos/100"
   profile_file = URI.open(profile_url)
   user.photo.attach(io: profile_file, filename: 'profile_img.png', content_type: 'image/png')
-end
-
-10.times do
-  item = Item.new(title: Faker::Appliance.equipment, description: Faker::Lorem.paragraph(sentence_count: 2), price: Faker::Number.between(from: 10.0, to: 80.0).round(2), deposit: rand(90..140), category: Faker::Commerce.department, user: user)
-  item.save
-  3.times do
-    item_url = "https://picsum.photos/300/200"
-    item_file = URI.open(item_url)
-    item.photo.attach(io: item_file, filename: "#{Item.title}.png", content_type: 'image/png')
+  2.times do
+    item = Item.new(title: Faker::Appliance.equipment, description: Faker::Lorem.paragraph(sentence_count: 2), price: Faker::Number.between(from: 10.0, to: 80.0).round(2), deposit: rand(90..140), category: Faker::Commerce.department, user: user)
+    item.save
+    3.times do
+      item_url = "https://picsum.photos/300/200"
+      item_file = URI.open(item_url)
+      item.photos.attach(io: item_file, filename: "item_img.png", content_type: 'image/png')
+    end
   end
 end
+
+
 
 # NEW
 puts '> Finished seeding!'

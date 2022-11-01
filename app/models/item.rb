5ad6b948e182ad010f13eadc3f0ce5d9b27a1256
Christoffer Nguyen <christofferhoang@gmail.com>
2022-11-01 20:26:00 +0200

    FEAT: Changed dashboard and navbar

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ApplicationRecord
   has_many_attached :photos
 
   belongs_to :user
+
+  validates :category, inclusion: %w[Stereo Tools Camera Art Surfboards Scubagear Grill Bike Utilities Camping Instruments Wilderness Other]
 end

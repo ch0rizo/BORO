b09f141b63a5d31479dd8339e5b2aa636d5f6ad3
Christoffer Nguyen <christofferhoang@gmail.com>
2022-11-01 17:06:53 +0200

    FEAT: Lend items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,7 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    @lending = Lending.new
   end
 
   def new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,5 @@
 class User < ApplicationRecord
   has_one_attached :photo
-  has_many :lendings
-
-  def unavailable_dates
-    bookings.pluck(:start_date, :end_date).map do |range|
-      { from: range[0], to: range[1] }
-    end
-  end
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable

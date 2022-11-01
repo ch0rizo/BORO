c291fbbaca8db1ed1099b90fbcd5e6a525edc8d7
Christoffer Nguyen <christofferhoang@gmail.com>
2022-11-01 12:52:53 +0200

    ARCH: Item create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,27 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @user = current_user
+    @item.user = @user
+    
+    if @item.save
+      redirect_to items_path, notice: 'Offer was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :user_id, :description, :deposit, :price, :category, :photos)
+  end
 end

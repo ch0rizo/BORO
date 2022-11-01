f8f556841cfd691e93bb8bf3b4b97e9b10dff770
Christoffer Nguyen <christofferhoang@gmail.com>
2022-11-02 00:28:34 +0200

    ARCH: added some validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ApplicationRecord
   has_one_attached :photo
   has_many :items
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, format: { with: /(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})/ }, allow_blank: true
+  validates :phone_number, uniqueness: true
+  validates :about, length: { maximum: 200 }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end

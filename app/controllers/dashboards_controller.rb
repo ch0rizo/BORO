a1cbb45e6ecafac5b08b974ac61245ef9c6b5576
Christoffer Nguyen <christofferhoang@gmail.com>
2022-11-02 15:13:22 +0200

    ARCH: Routes FUNC: Accept-Decline

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,6 +4,8 @@ class DashboardsController < ApplicationController
   end
 
   def updated
-    
+    @user = current_user
+    @lending = Lending.find(@user)
+    @lending.update(status: 1)
   end
 end

b8579be4d22bd9df5a13029986c35ad1d65ac8db
Christoffer Nguyen <christofferhoang@gmail.com>
2022-11-03 11:54:01 +0200

    FIX: seed and truncate about

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
   validates :first_name, :last_name, :email, presence: true
   validates :email, :phone_number, uniqueness: true
   validates :phone_number, format: { with: /(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})/ }, allow_blank: true
-  validates :about, length: { maximum: 300 }
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable

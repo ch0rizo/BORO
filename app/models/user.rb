96863122c94f9189f2dd4256e0d2c71f467e1d4b
Christoffer Nguyen <christofferhoang@gmail.com>
2022-11-03 11:20:08 +0200

    CONF: validation lending, item, user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,10 @@ class User < ApplicationRecord
   has_many :requests, through: :items, source: :lendings
   has_many :reviews, through: :lendings
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :first_name, :last_name, :email, presence: true
+  validates :email, :phone_number, uniqueness: true
   validates :phone_number, format: { with: /(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})/ }, allow_blank: true
-  validates :phone_number, uniqueness: true
-  validates :about, length: { maximum: 200 }
+  validates :about, length: { maximum: 300 }
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
